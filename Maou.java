aab35a22622e6b3e285474dfce428543b1f1b295
unknown <c0116040f6@edu.teu.ac.jp>
2016-10-27 20:19:52 +0900

    first

---
+++ Maou.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Maou {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("魔王に会いました");
+
+		M m = new M();
+
+
+		while(true){
+			int a = sc.nextInt();
+			m.setHp(m.getHp()-a);
+			if(m.hp <= 0){
+				break;
+			}
+		}
+		System.out.println("勇者は世界を救った");
+	}
+}
+
+class M{
+	int hp  = 10;
+
+	void setHp(int hp){
+		this.hp = hp;
+	}
+
+	int getHp(){
+		return this.hp;
+	}
+}
